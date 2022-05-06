8060acdaec940fba8781775ead3f02be594ea7c2
rnrlgus <rnrl4611@gmail.com>
2022-05-06 14:49:26 +0900

    tree

--- problem_solving_techniques/tree/tree_3.py
+++ problem_solving_techniques/tree/tree_3.py
@@ -1,1 +1,78 @@
-# 타겟 노드부터 부모 찾아가면서 루트까지??
+# preOrder 순회를 통해 얻은 preorder 리스트와 이를 통해 얻은 preorder 순서의 cost 리스트를 구한 뒤
+# k번 노드의 subtree를 구하기 위해 dfs 탐색을 진행해 subtree의 노드 수 를 구했다.
+# 앞의 정보들을 활용해 subtree의 코스트를 계산했다. 
+# update 쿼리의 경우 preorder 리스트에서 업데이트할 노드의 인덱스를 찾은 뒤 cost 리스트에서 update해줬다. 
+# 총 시간복잡도: O(n + n-1)
+
+
+# 엣지들을 통해 각 인덱스가 노드의 변호가 되고 각 자식 노드들을 원소로 갖는 리스트 만들기
+def makeTree(edges): # 루트 노드는 1번 노드
+    # O(n)
+    tree = [[] for _ in range(n+1)]
+    # O(n)
+    for i in edges:
+        tree[i[0]].append(i[1])
+
+    return tree
+
+# preorder 함수의 경우 인접 리스트 형태의 그래프를 dfs를 사용해 탐색했기 때문에 둘다
+# 시간복잡도의 경우 O(n+(n-1))이다. (n개의 노드, n-1개의 edges)   
+def preOrder(tree, v, visited):
+    visited[v] = True
+    pre.append(v)
+    for i in tree[v]:
+        if not visited[i]:
+            preOrder(tree, i, visited)
+            
+def getSubnum(tree, k, visited):
+    global tmp
+    visited[k] = True
+    pathSum[k] = tmp + cost[k]
+    
+    for i in tree[k]:
+        if not visited[i]:
+            tmp += cost[k]
+            
+            getSubnum(tree, i, visited)
+    tmp = 0
+
+#main
+
+#input
+n, q = map(int, input().split())
+cost = [0]
+cost += list(map(int, input().split()))
+edges = []
+for i in range(n-1):
+    edges.append(list(map(int, input().split())))
+queries = []
+for i in range(q):
+    queries.append(input())
+
+# variables
+pre = [0]
+pathSum = [0 for _ in range(n+1)]
+tmp = 0
+
+tree = makeTree(edges)
+visited = [False]*(n+1)
+preOrder(tree, 1, visited)
+
+print("-------------")
+print("cost: ",cost)
+print("pre: ",pre)
+print("-------------")
+visited = [False]*(n+1)
+getSubnum(tree, 1, visited)
+print(pathSum)
+
+#print(queries)
+# for i in queries:
+#     if i[0] == "s":
+#         #print("query: ",i)
+#         s, root = i.split()
+#         tmp = []
+#         print(getSubcost(tree, int(root)))
+#     else:
+#         u, f, t = i.split()
+#         preCost[pre.index(int(f))] += int(t)
---
+++ problem_solving_techniques/tree/tree_4.py
@@ -0,0 +1,74 @@
+import sys 
+from collections import deque
+
+def make_tree(v, edge_raw, root):
+    edge = [[] for _ in range(v+1)]
+    for a, b in edge_raw: 
+        edge[a].append(b)
+        edge[b].append(a)
+
+    check, depth, tree = [[0 for _ in range(v+1)] for _ in range(3)]
+    check[root] = True
+
+    q = deque([root])
+    while len(q):
+        now = q.popleft()
+        for nxt in edge[now]:
+            if check[nxt] : continue
+            depth[nxt] = depth[now] + 1
+            check[nxt] = True
+            tree[nxt] = now
+            q.append(nxt)
+
+    return tree, depth
+
+def sparse_tree(v, tree):
+    p = [[0 for _ in range(18)] for _ in range(v+1)]
+    for i in range(1, v+1): p[i][0] = tree[i]
+    bit = 1
+    while 2**bit < v :
+        for i in range(1, v+1):
+            if p[i][bit-1] != 0:
+                p[i][bit] = p[p[i][bit-1]][bit-1]
+        bit+=1
+    return p
+
+
+def lca(tree, depth, sparse, u, v):
+    if depth[u] < depth[v] : u, v = v, u
+    bit = 1
+    while 2**(bit-1) <= depth[u] : bit+=1
+    
+    for i in range(bit, -1, -1) : 
+        if depth[u] - 2**i >= depth[v]: u = sparse[u][i]
+
+    if u == v : return u
+    for i in range(bit, -1, -1):
+        if sparse[u][i] != 0 and sparse[u][i] != sparse[v][i]: 
+            u, v = sparse[u][i], sparse[v][i]
+    return tree[u]
+
+# main
+n, q = map(int, input().split())
+edges = []
+queries = []
+for i in range(n-1):
+    edges.append(list(map(int, input().split())))
+for i in range(q):
+    queries.append(list(map(int, input().split())))
+    
+print(edges)
+print(queries)
+    
+
+# if __name__ == '__main__':
+#     input = sys.stdin.readline
+#     N = int(input())
+#     edge = [tuple(map(int, input().split())) for _ in range(N-1)]
+#     tree, depth = make_tree(N, edge, 1)
+#     sparse = sparse_tree(N, tree)
+
+#     M = int(input())
+#     for _ in range(M):
+#         u, v = map(int, input().split())
+#         print(lca(tree, depth, sparse, u, v))
