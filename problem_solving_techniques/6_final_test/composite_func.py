76d2b7c80d594b880debfdf0ec181baea4170d3f
rnrlgus <rnrl4611@gmail.com>
2022-06-10 13:46:41 +0900

    합성함수, 차선책

--- problem_solving_techniques/6_final_test/composite_func.py
+++ problem_solving_techniques/6_final_test/composite_func.py
@@ -1,3 +1,7 @@
+# 단순하게 DP로 구현했을 때 시간 복잡도의 비 효율성을 고려해 sparse table을 만들고
+# 활용하는 방식으로 시간 복잡도를 효율적으로 만들었다.
+# 시간복잡도: O(log(n))
+
 n = int(input()) # 1 ~ 10000
 fx = [0]
 fx += list(map(int, input().split()))
@@ -9,11 +13,12 @@ log = 14 # 2**13 = 8192
 
 for j in range(1, log + 1):
     for i in range(1, n+1):
-        dp[i].append(dp[dp[i][j-1]][j-1])
+        dp[i].append(dp[dp[i][j-1]][j-1]) # 점화식
 
 for _ in range(query_num):
     x, k = map(int, input().split())
     
+    # 매 쿼리 때마다 shift를 해주며 값이 0이 되기 전까지 반복
     for i in range(log, -1, -1):
         if k >= 1 << i:
             k-=1 << i
@@ -21,12 +26,3 @@ for _ in range(query_num):
     print( x)
     
 
-
-print("----------------------------")
-# print("n: ",n)
-# print("f(x): ",fx)
-# print("query_num = ",query_num)
-# print("x: ", x)
-# print("k: ", k)
-
-print(dp)
--- problem_solving_techniques/6_final_test/second_bestWay.py
+++ problem_solving_techniques/6_final_test/second_bestWay.py
@@ -1,6 +1,15 @@
+# 다익스트라 알고리즘을 활용했다. 
+# 기본적으로 다익스트라와 동일하게 최단거리로 갱신해주며 
+# 2번째 최단 거리를 알아내기 위해 n * 2 크기의 이차원 배열을 활용했다.
+# 위의 이차원 배열에 최단 거리 순의 경로를 삽입하고 2번째 원소를 모두 더했다.
+# 또한 문제의 조건을 만족하기 위해 2번째 원소가 inf일 경우 그 노드로의 경로는 배제했다.
+
+# 시간복잡도: O(n + m * log(n) )
+
 import sys
 from heapq import heappush, heappop
 
+# O(n + m * log(n) )
 def dijkstra():
     heappush(heap, [0, 0])
     
@@ -20,8 +29,6 @@ def dijkstra():
                 distance[i].sort()
                 heappush(heap, [w,i])
 
-    
-
 #main
 n = int(input()) # 노드 개수 ~500
 m = int(input()) # 엣지 개수 ~10000
@@ -37,7 +44,9 @@ heap = []
 
 dijkstra()
 
-print(distance)
-
-print("------------------")
-print(graph)
+sum = 0
+for i in distance:
+    if i[1] != inf:
+        sum += i[1]
+        
+print(sum)
