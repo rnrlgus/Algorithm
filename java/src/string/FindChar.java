83088db97bbbfa853aedd4380ef81f3d1f51e625
rnrlgus <rnrl4611@gmail.com>
2022-09-02 22:34:17 +0900

    test

--- java/src/string/FindChar.java
+++ java/src/string/FindChar.java
@@ -1,6 +1,8 @@
+package string;
+
 import java.util.Scanner;
 
-public class Main {
+public class FindChar {
 
     public int solution(String str, char c) {
         int answer = 0;
@@ -8,8 +10,14 @@ public class Main {
         str = str.toUpperCase();
         c = Character.toUpperCase(c);
 
-        for (int i = 0; i < str.length(); ++i) {
-            if (str.charAt(i) == c) {
+//        for (int i = 0; i < str.length(); ++i) {
+//            if (str.charAt(i) == c) {
+//                answer += 1;
+//            }
+//        }
+
+        for (char x : str.toCharArray()) {
+            if (x == c) {
                 answer += 1;
             }
         }
@@ -18,13 +26,13 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Main T = new Main();
+        FindChar findChar = new FindChar();
         Scanner scan = new Scanner((System.in));
         String str = scan.next();
         char c = scan.next().charAt(0);
 
 
-        System.out.println(T.solution(str, c));
+        System.out.println(findChar.solution(str, c));
 
     }
 }
