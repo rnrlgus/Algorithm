38f795d7207633efc681d3d551276380f864d280
rnrlgus <rnrl4611@gmail.com>
2022-09-14 20:26:41 +0900

    array end

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,53 +2,48 @@ import java.util.Scanner;
 
 public class Main {
 
-    public int solution(int n, int[][] arr) {
+    public int solution(int n, int m, int[][] arr) {
         int answer = 0;
 
-        int[][] result = new int[n][n];
-        int[] tmp = new int[n];
-
-
-        for (int j = 0; j < 5; ++j) { // 학년별 반복
-            for (int k = 0; k < n; ++k) { // 학생1
-                for (int l = 0; l < n; ++l) { // 학생2
-                    if (k != l && arr[k][j] == arr[l][j]) {
-                        result[k][l] = 1;
+        for (int i = 1; i <= n; ++i) { // 학생1 번호
+            for (int j = 1; j <= n; ++j) { // 학생2 번호
+                int cnt = 0;
+                for (int k = 0; k < m; ++k) { // 테스트 횟수
+                    int idxI=0;
+                    int idxJ=0;
+                    for (int s = 0; s < n; ++s) { // i번, j번 학생 인덱스 체크 후 멘토멘티 성립 조건 확인
+                        if (arr[k][s] == i) {
+                            idxI = s;
+                        }
+                        if (arr[k][s] == j) {
+                            idxJ = s;
+                        }
                     }
+                    if (idxI < idxJ) {
+                        cnt++;
+                    }
+                }
+                if (cnt == m) {
+                    answer ++;
                 }
             }
         }
 
-        int cnt;
-        for (int i = 0; i < n; ++i) {
-            cnt = 0;
-            for (int j = 0; j < n; ++j) {
-                if (result[i][j] == 1) cnt++;
-            }
-            tmp[i] = cnt;
-        }
-
-        int maxIdx = 0;
-        for (int i = 1; i < n; ++i) {
-            if (tmp[i] > tmp[maxIdx]) {
-                maxIdx = i;
-            }
-        }
-        answer += maxIdx + 1;
         return answer;
     }
 
     public static void main(String[] args) {
         Main main = new Main();
         Scanner scan = new Scanner((System.in));
-        int n = scan.nextInt();
-        int[][] map = new int[n][5];
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < 5; ++j) {
+        int n = scan.nextInt(); // 학생 수
+        int m = scan.nextInt(); // 테스트 수
+        int[][] map = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
                 map[i][j] = scan.nextInt();
             }
         }
 
-        System.out.println(main.solution(n, map));
+        System.out.println(main.solution(n, m, map));
     }
 }
