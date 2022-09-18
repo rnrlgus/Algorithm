b40326b5d688b4b1a24a4bf05a73d58fc14ae3b8
rnrlgus <rnrl4611@gmail.com>
2022-09-18 20:13:54 +0900

    prefix/postfix operator

--- java/src/Test.java
+++ java/src/Test.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+
 public class Test {
     public static void main(String[] args) {
-        int p1=0, p2 = 0;
-        System.out.println(p1 + " " + p2);
+        int[] arr = new int[] {1, 2, 3, 4, 5};
+        ArrayList<Integer> al = new ArrayList<>();
+        int p = 0;
+        while(p< arr.length) al.add(arr[p++]); // 1 2 3 4 5
+
+        while (p < arr.length-1) al.add(arr[++p]); // 2 3 4 5
+
+        for (int x : al) System.out.print(x + " ");
+
     }
 }
