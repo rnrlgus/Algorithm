ad22ccb231048b5710e8fd9900186834ca3ce108
rnrlgus <rnrl4611@gmail.com>
2022-10-29 20:47:26 +0900

    same sum subsets dfs

--- java/src/dfs/SameSumSubsets.java
+++ java/src/dfs/SameSumSubsets.java
@@ -1,7 +1,8 @@
+package dfs;
 
-import java.util.*;
+import java.util.Scanner;
 
-public class Main  {
+public class SameSumSubsets {
 
     static int n;
     static int[] check;
@@ -37,7 +38,7 @@ public class Main  {
      }
 
     public static void main(String[] args) {
-        Main main = new Main();
+        SameSumSubsets main = new SameSumSubsets();
         Scanner scan = new Scanner(System.in);
 
         n = scan.nextInt();
